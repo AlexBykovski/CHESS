35abc8d103d9e2721d2756d0c446b35f011d5dcf
slavchoo <slavchoo2@gmail.com>
2015-05-16 01:11:19 +0300

    add register

--- frontend/register-page/register-controller.js
+++ frontend/register-page/register-controller.js
@@ -1,2 +1,7 @@
 chessApp.controller('RegisterCtrl', ['$scope', '$location', function ($scope, $location) {
+    $scope.user = {};
+
+    $scope.registerClick = function(){
+        console.log($scope.user);
+    }
 }]);
